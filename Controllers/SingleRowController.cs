4eb9ce949f63a4daccc68ec3cd52006de98b0f8c
Samuel Fernandes <Samuel.Fernandes@devops.ncia.nato.int>
2025-02-14 11:05:20 +0100

    adding single row controller

---
+++ Controllers/SingleRowController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using CoreLibrary.Core.Entities;
+using CoreLibrary.Core.Interfaces;
+using CoreLibrary.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CoreLibrary.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SingleRowController<TForm, TDto, TEntity>(IGenericService<IRepository<TEntity>, TDto, TEntity> service,
+        IMapper mapper, ILogger<SingleRowController<TForm, TDto, TEntity>> logger) : ControllerBase
+        where TEntity : BaseEntity, IAggregateRoot
+        where TDto : BaseDto
+        where TForm : BaseForm
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await service.Get(Guid.Empty);
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(TForm form)
+        {
+            try
+            {
+                var dto = mapper.Map<TDto>(form);
+
+                var result = await service.Update(Guid.Empty, dto);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                var innerException = ex.InnerException?.Message;
+
+                logger.LogError(innerException ?? ex.Message);
+
+                return StatusCode(500, new { ex.Message, innerException });
+            }
+        }
+    }
+}
