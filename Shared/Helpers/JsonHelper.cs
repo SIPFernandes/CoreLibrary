e2df482dd0e149457300bc31139f70195f317239
Samuel Fernandes <Samuel.Fernandes@devops.ncia.nato.int>
2025-03-13 12:37:33 +0100

    improve json helper

--- Shared/Helpers/JsonHelper.cs
+++ Shared/Helpers/JsonHelper.cs
@@ -4,18 +4,32 @@ namespace CoreLibrary.Shared.Helpers
 {
     public static class JsonHelper
     {
-        private static JsonSerializerOptions options = new()
+        private static readonly JsonSerializerOptions _options = new()
         {
             PropertyNameCaseInsensitive = true
         };
 
         public static string Serialize(this object obj)
         {
+
             return JsonSerializer.Serialize(obj);
+        }
+
+        public static string Serialize(this object obj, JsonSerializerOptions? options = null)
+        {
+
+            return JsonSerializer.Serialize(obj, options);
         }
 
         public static T? Deserialize<T>(this string json)
         {
+            return JsonSerializer.Deserialize<T>(json, _options);
+        }
+
+        public static T? Deserialize<T>(this string json, JsonSerializerOptions? options = null)
+        {
+            options ??= _options;
+
             return JsonSerializer.Deserialize<T>(json, options);
         }
     }
