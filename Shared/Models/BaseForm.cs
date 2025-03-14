bea994a53d72c412f59cb3aded860c38e00b38cd
Samuel Fernandes <Samuel.Fernandes@devops.ncia.nato.int>
2025-03-14 14:57:51 +0100

    remove from required creator Id in BaseForm

--- Shared/Models/BaseForm.cs
+++ Shared/Models/BaseForm.cs
@@ -4,6 +4,6 @@ namespace CoreLibrary.Shared.Models
 {
     public abstract class BaseForm : IBinderInterface
     {
-        public required virtual Guid CreatorId { get; set; }
+        public virtual Guid CreatorId { get; set; }
     }
 }
