51f7933aad631f721e12e55d0ee2b46184c31243
Samuel Fernandes <Samuel.Fernandes@devops.ncia.nato.int>
2025-07-30 13:17:48 +0200

    added FilterModel operator enum

--- Shared/Filters/ControllerFilterModels/FilterModels/FilterModel.cs
+++ Shared/Filters/ControllerFilterModels/FilterModels/FilterModel.cs
@@ -1,9 +1,19 @@
-﻿namespace CoreLibrary.Shared.Filters.ControllerFilterModels.FilterModels
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreLibrary.Shared.Filters.ControllerFilterModels.FilterModels
 {
     public class FilterModel
     {
         public required string PropertyName { get; set; }
+        [DefaultValue(nameof(OperatorEnum.Equal))]
+        [EnumDataType(typeof(OperatorEnum))]
         public required string Operator { get; set; }
         public required string? Value { get; set; }
+    }
+
+    public enum OperatorEnum
+    {
+        Equal, NotEqual, GreaterThan, GreaterThanOrEqual, LessThan, LessThanOrEqual, Contains, IsNull, IsNotNull
     }
 }
