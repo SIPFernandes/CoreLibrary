634df3c009fb4a51881879e68070a9fc0f26f211
Samuel Fernandes <Samuel.Fernandes@devops.ncia.nato.int>
2025-09-07 12:48:15 +0200

    rename param base dto

--- Shared/Models/BaseDto.cs
+++ Shared/Models/BaseDto.cs
@@ -3,14 +3,14 @@ using System.ComponentModel;
 
 namespace CoreLibrary.Shared.Models
 {
-    public abstract class BaseDto(Guid userId) : IBinderInterface
+    public abstract class BaseDto(Guid creatorId) : IBinderInterface
     {
         [DefaultValue("00000000-0000-0000-0000-000000000000")]
         public virtual Guid Id { get; set; }
         [DefaultValue(false)]
         public bool IsDeleted { get; set; } = false;
-        public virtual Guid CreatorId { get; set; } = userId;
-        public virtual Guid UpdatedById { get; set; } = userId;
+        public virtual Guid CreatorId { get; set; } = creatorId;
+        public virtual Guid UpdatedById { get; set; } = creatorId;
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
     }
